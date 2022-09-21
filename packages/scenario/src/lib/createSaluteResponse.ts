e7b83191857f10ccaebeb5c6d970f9768091fd61
Alexander Salmin <sasha-tlt@yandex.ru>
2022-09-21 15:40:07 +0300

    fix: Заполнять поле framework_type в ответе

--- packages/scenario/src/lib/createSaluteResponse.ts
+++ packages/scenario/src/lib/createSaluteResponse.ts
@@ -16,6 +16,7 @@ export const createSaluteResponse = (req: NLPRequest): SaluteResponse => {
             items: [],
             finished: false,
             intent: 'scenario',
+            framework_type: 'salutejs',
         },
     };
 

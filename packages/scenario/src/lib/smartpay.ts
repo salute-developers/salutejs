add0b60132ecab68963f03f935c29ebde2a23a5a
Alexander Salmin <sasha-tlt@yandex.ru>
2022-12-20 16:33:04 +0300

    feat: Возможность скипать проверку сертификата smartpay

--- packages/scenario/src/lib/smartpay.ts
+++ packages/scenario/src/lib/smartpay.ts
@@ -1,12 +1,21 @@
 import fetch from 'node-fetch';
+import { Agent } from 'https';
 
 import { PaymentInvoiceQuery, PaymentInvoiceAnswer, PaymentResponse, PaymentStatus } from './types/payment';
 
 const API_URL = 'https://smartmarket.online.sberbank.ru/smartpay/v1';
 
+const agent = new Agent({
+    rejectUnauthorized: false,
+});
+
 const callApi = <T>(
     url: string,
-    { method, body }: { method?: 'patch' | 'post' | 'put' | 'delete'; body?: unknown } = {},
+    {
+        method,
+        body,
+        sslVerify = true,
+    }: { method?: 'patch' | 'post' | 'put' | 'delete'; body?: unknown; sslVerify?: boolean } = {},
 ): Promise<T> =>
     fetch(url, {
         headers: {
@@ -16,6 +25,7 @@ const callApi = <T>(
         },
         method: method || 'get',
         body: body ? JSON.stringify(body) : undefined,
+        agent: sslVerify ? undefined : agent,
     }).then((response) => response.json());
 
 /**
@@ -23,10 +33,11 @@ const callApi = <T>(
  * @param invoice объект счета
  * @returns объект-статус регистрации платежа
  */
-export const createInvoice = (invoice: PaymentInvoiceQuery): Promise<PaymentInvoiceAnswer> =>
+export const createInvoice = (invoice: PaymentInvoiceQuery, sslVerify?: boolean): Promise<PaymentInvoiceAnswer> =>
     callApi<PaymentInvoiceAnswer>(`${API_URL}/invoices`, {
         method: 'post',
         body: invoice,
+        sslVerify,
     });
 
 /**
@@ -38,6 +49,7 @@ export const createInvoice = (invoice: PaymentInvoiceQuery): Promise<PaymentInvo
 export const findInvoiceById = (
     invoiceId: string,
     params: { invStatus?: string; wait: number },
+    sslVerify?: boolean,
 ): Promise<PaymentStatus> =>
     callApi(
         `${API_URL}/invoices/${encodeURIComponent(invoiceId)}${
@@ -47,6 +59,7 @@ export const findInvoiceById = (
                   }`
                 : ''
         }`,
+        { sslVerify },
     );
 
 /**
@@ -60,6 +73,7 @@ export const findInvoiceByServiceIdOrderId = (
     serviceId: string,
     orderId: string,
     params: { invStatus?: string; wait?: number } = {},
+    sslVerify?: boolean,
 ): Promise<PaymentStatus> =>
     callApi(
         `${API_URL}/invoices/0?service_id=${encodeURIComponent(serviceId)}&order_id=${encodeURIComponent(orderId)}${
@@ -68,6 +82,7 @@ export const findInvoiceByServiceIdOrderId = (
                 params.wait ?? `&wait=${encodeURIComponent(!params.wait)}`
             }`
         }`,
+        { sslVerify },
     );
 
 /**
@@ -75,9 +90,10 @@ export const findInvoiceByServiceIdOrderId = (
  * @param invoiceId идентификатор счета
  * @returns объект-результат запроса
  */
-export const completeInvoice = (invoiceId: string): Promise<PaymentResponse> =>
+export const completeInvoice = (invoiceId: string, sslVerify?: boolean): Promise<PaymentResponse> =>
     callApi(`${API_URL}/invoice/${encodeURIComponent(invoiceId)}`, {
         method: 'put',
+        sslVerify,
     });
 
 /**
@@ -85,9 +101,10 @@ export const completeInvoice = (invoiceId: string): Promise<PaymentResponse> =>
  * @param invoiceId идентификатор счета
  * @returns объект-результат запроса
  */
-export const reverseInvoice = (invoiceId: string): Promise<PaymentResponse> =>
+export const reverseInvoice = (invoiceId: string, sslVerify?: boolean): Promise<PaymentResponse> =>
     callApi(`${API_URL}/invoice/${encodeURIComponent(invoiceId)}`, {
         method: 'delete',
+        sslVerify,
     });
 
 /**
@@ -95,7 +112,8 @@ export const reverseInvoice = (invoiceId: string): Promise<PaymentResponse> =>
  * @param invoiceId идентификатор счета
  * @returns объект-результат запроса
  */
-export const refundInvoice = (invoiceId: string): Promise<PaymentResponse> =>
+export const refundInvoice = (invoiceId: string, sslVerify?: boolean): Promise<PaymentResponse> =>
     callApi(`${API_URL}/invoice/${encodeURIComponent(invoiceId)}`, {
         method: 'patch',
+        sslVerify,
     });

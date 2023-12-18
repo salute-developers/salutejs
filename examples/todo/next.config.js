f3b1b8bf84328cdaa5b34b95845e244e52aa0ba6
Evgeniy Semin <evgeniy.o.semin@gmail.com>
2023-12-18 11:33:30 +0300

    test: Тест

--- examples/todo/next.config.js
+++ examples/todo/next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
+    swcMinify: false,
     compiler: {
         // Enables the styled-components SWC transform
-        styledComponents: true
-    }
-}
+        styledComponents: true,
+    },
+};
